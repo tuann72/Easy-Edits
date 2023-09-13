08b751e3caecaa7cfe3222d009cf27ffb78b134b
Tuan Nguyen <tuannguyen08426@gmail.com>
2023-09-12 22:04:55 -0500

    Created default settings.

---
+++ settings.py
@@ -0,0 +1,20 @@
+# Default Values
+ROTATE_DEFAULT = 0
+ZOOM_DEFAULT = 0
+FLIP_OPTIONS = ["None", "X", "Y", "Both"]
+BLUR_DEFAULT = 0
+CONTRAST_DEFAULT = 0
+EFFECT_OPTIONS = ["None", "Emboss", "Find Edges", "Contour", "Edge Enhance"]
+BRIGHTNESS_DEFAULT = 1
+VIBRANCE_DEFAULT = 1
+GRAYSCALE_DEFAULT = False
+INVERT_DEFAULT = False
+
+# Colors
+WHITE = "#FFF"
+GREY = "grey"
+BLUE = "1f6aa5"
+DARK_GREY = "#4a4a4a"
+CLOSE_RED = "8a0606"
+
+SLIDER_BG = "#64686b"
